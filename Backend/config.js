da1bca0f8177488b63d002261ca76205e15d3c31
Himani Sharma <96700396+himani1001@users.noreply.github.com>
2025-06-18 23:20:47 +0530

    Update config.js

--- Backend/config.js
+++ Backend/config.js
@@ -1,4 +1,4 @@
 export const PORT = process.env.PORT || 5556;
 
 export const  mongoDBURL = 
-'mongodb+srv://himani:nveorguegjgsb@cluster0.wudibby.mongodb.net/tbr-and-other-books-collection?retryWrites=true&w=majority&appName=Cluster0'
+'your-url'
